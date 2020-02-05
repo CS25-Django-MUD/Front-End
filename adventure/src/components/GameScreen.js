394c4ecf48b094c8cc2dc111cb10ba20a33003b4
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-02-04 21:05:58 -0500

    Sketched the Room componenet so we can display data from the backend API.

--- adventure/src/components/GameScreen.js
+++ adventure/src/components/GameScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Room from './Room';
+import './styles.css'
+
+//I think I'll make an axios call here, receive the JSON object, store the response data to state (useState), then map through it & render a Room object for each.
+
+// const addRooms = () => {
+
+// }
 
 const GameScreen = () => {
     return (
-        <div>
-            
+        <div className="rooms-grid">
+            <Room name={'Hallway'}/>
+            <h3>--------</h3>
+            <Room name={'Foyer'}/>
+            <h3>--------</h3>
+            <Room name={'Kitchen'}/>
         </div>
     )
 }
