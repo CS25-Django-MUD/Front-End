fe06fdf4838b9fe288e3830e285073038a7a0168
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-02-05 18:55:34 -0500

    Login and Registration paths are saving to the deployed server and storing new users

--- adventure/src/App.js
+++ adventure/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
 import PrivateRoute from './components/PrivateRoute';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import GameScreen from './components/GameScreen';
@@ -11,11 +12,12 @@ function App() {
     <div className="App">
       <ul className="navbar">
         <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
         <Link to="/dashboard">Play here!</Link>
       </ul>
 
-      <div>Haven't registered yet?....</div>
       <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
       <PrivateRoute path="/dashboard" component={GameScreen} />
     </div>
     </Router>
--- adventure/src/components/Register.js
+++ adventure/src/components/Register.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useState} from 'react';
+import axiosWithAuth from './axiosWithAuth';
 
 const Register = () => {
+
+    const [credentials, setCredentials] = useState({username: '', password1: '', password2: ''});
+
+    const changeHandler = e => {
+        setCredentials({
+          ...credentials, 
+          [e.target.name]: e.target.value,
+        });
+      }
+
+    const signupHandler = e => {
+        e.preventDefault();
+        //Deployed server
+        axiosWithAuth().post('https://cs25-mud.herokuapp.com/api/registration/', credentials)
+        //Test server
+        // axiosWithAuth().post('https://lambda-mud-test.herokuapp.com/api/registration/', credentials)
+        .then(res => {
+          console.log('Logged in result', res);
+      })
+        .catch(err => {
+        console.log(err)
+      })
+      }
+
     return (
-        <>
-        </>
+        <div>
+            <form onSubmit={signupHandler}>
+                <label>Username: 
+                <input
+                type="text"
+                name="username"
+                value={credentials.username}
+                onChange={changeHandler}
+                >
+                </input>
+                </label>
+
+                <label>Password: 
+                <input
+                type="password"
+                name='password1'
+                value={credentials.password1}
+                onChange={changeHandler}>
+                </input>
+
+                <input
+                type="password"
+                name='password2'
+                value={credentials.password2}
+                onChange={changeHandler}>
+                </input>
+
+                </label>
+                <button type="submit">Register</button>
+            </form>
+        </div>
     )
 }
 
