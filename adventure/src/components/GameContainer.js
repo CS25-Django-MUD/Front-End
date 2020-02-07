796cf5435caa2915c16030d7fae9e089971e009e
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2020-02-06 19:59:42 -0500

    Move endpoint is close to finished. Second Auth function added to axioswithauth. Added some more helper functions.

--- adventure/src/components/GameContainer.js
+++ adventure/src/components/GameContainer.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import axiosWithAuth from './axiosWithAuth';
+import {axiosWithAuth, axiosWithAuthToken} from './axiosWithAuth';
 import './styles.css';
 import GameScreen from './GameScreen';
 
 
 const GameContainer = (props) => {
     const [rooms, setRooms] = useState({})
+    const [playerRoom, setPlayerRoom] = useState();
     // let user = JSON.parse(props.usercreds.config.data)
+    let currentRoom;
 
+    
     const addRooms = () => {
         axiosWithAuth().get('https://cs25-mud.herokuapp.com/api/adv/rooms/')
         .then(res => {
+            // let myRooms = res.data.sort((a, b) => (a.id > b.id) ? 1 : -1)
             setRooms(res);
             console.log(res);
             })
@@ -18,27 +22,48 @@ const GameContainer = (props) => {
         }
 
     const startGame = () => {
-        axiosWithAuth().get('https://cs25-mud.herokuapp.com/api/adv/init')
+        axiosWithAuthToken().get('https://cs25-mud.herokuapp.com/api/adv/init')
+        .then(res => {
+            console.log(res)
+            let roomid = res.data.roomid
+            setPlayerRoom(roomid);
+        })
+        .catch(err => console.log(err));
+    }
+
+    const movePlayer = e => {
+        currentRoom = rooms.data.filter(i => {
+            return i.id === playerRoom;
+        })
+        let direction = JSON.stringify({'direction': e.target.value})
+        console.log(direction);
+        axiosWithAuthToken().post('https://cs25-mud.herokuapp.com/api/adv/move', direction)
         .then(res => {
             console.log(res)
         })
-        // .then(() => {
-        //     axiosWithAuth().get('https://cs25-mud.herokuapp.com/api/adv/move')
-        // })
         .catch(err => console.log(err));
+        // currentRoom = rooms.data.filter(i => {
+        //     return i.id === playerRoom;
+        // })
     }
 
     useEffect(() => {
         addRooms()
     }, [])
     if (props.usercreds !== undefined) {
-        // console.log("Usercreds are ", user, user.username);
     }
+
     return (
         <div className="game-container">
             <button onClick={startGame} className="start-button">Start playing!</button>
             <div className="rooms-grid">
-                <GameScreen rooms={rooms}/>
+                <GameScreen rooms={rooms} playerRoom={playerRoom}/>
+            </div>
+            <div>
+                <button className="direction-button" value="n" onClick={movePlayer}>N</button>
+                <button className="direction-button" value="e" onClick={movePlayer}>E</button>
+                <button className="direction-button" value="s" onClick={movePlayer}>S</button>
+                <button className="direction-button" value="w" onClick={movePlayer}>W</button>
             </div>
         </div>
     )
